68d4c01be4d2c3c80bf92cd0716af51dd0253d77
Dan Fellner <dfellner92@gmail.com>
2020-10-06 17:29:38 -0400

    currently have mern endpoints for CRU

--- client/src/App.js
+++ client/src/App.js
@@ -11,11 +11,22 @@ function App() {
     <Router>
       <div className="Container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar brand" target="_blank">
-            <img src={logo} width="30" height="30" />
+          <a className="navbar brand" href="https://www.codingthesmartway.com" target="_blank" rel="noopener noreferrer">
+            <img src={logo} width="30" height="30" alt="codingthesmartway.com"/>
           </a>
+          <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">Todos</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">Create Todo</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
-        <Route exact path="/" exact component={TodoList}></Route>
+        <Route path="/" exact component={TodoList}></Route>
         <Route path="/edit/:id" component={EditTodo}></Route>
         <Route path="/create"  component={CreateTodo}></Route>
       </div>  
